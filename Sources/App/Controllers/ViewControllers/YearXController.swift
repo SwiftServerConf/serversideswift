da10a39167838f0288291542e650182970d656b1
Steffen D. Sommer <steffendsommer@icloud.com>
2019-05-12 17:59:25 +0200

    Add FAQ page

--- Sources/App/Controllers/ViewControllers/YearXController.swift
+++ Sources/App/Controllers/ViewControllers/YearXController.swift
@@ -2,16 +2,26 @@ import Vapor
 import Fluent
 
 struct YearXController: RouteCollection {
-  func boot(router: Router) throws {
-    router.get(use: homepageHandler)
-  }
-  
-  func homepageHandler(_ req: Request) throws -> Future<View> {
-    let homeContext = HomeContext()
-    return try req.view().render("App/YearX/Pages/Home/home", homeContext)
-  }
+    func boot(router: Router) throws {
+        router.get(use: homepageHandler)
+        router.get("faq", use: faqHandler)
+    }
+
+    func homepageHandler(_ req: Request) throws -> Future<View> {
+        let homeContext = HomeContext()
+        return try req.view().render("App/YearX/Pages/Home/home", homeContext)
+    }
+
+    func faqHandler(_ req: Request) throws -> Future<View> {
+        let faqContext = FaqContext()
+        return try req.view().render("App/YearX/Pages/FAQ/faq", faqContext)
+    }
 }
 
 struct HomeContext: Encodable {
-  let page = ["home": true]
+    let page = ["home": true]
+}
+
+struct FaqContext: Encodable {
+    let page = ["faq": true]
 }
