1208ef28e11da668d90dbe73906a57e2a68532bb
Steffen D. Sommer <steffendsommer@icloud.com>
2019-05-12 18:04:48 +0200

    Add COC

--- Sources/App/Controllers/ViewControllers/YearXController.swift
+++ Sources/App/Controllers/ViewControllers/YearXController.swift
@@ -5,6 +5,7 @@ struct YearXController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: homepageHandler)
         router.get("faq", use: faqHandler)
+        router.get("code-of-conduct", use: codeOfConductHandler)
     }
 
     func homepageHandler(_ req: Request) throws -> Future<View> {
@@ -16,6 +17,11 @@ struct YearXController: RouteCollection {
         let faqContext = FaqContext()
         return try req.view().render("App/YearX/Pages/FAQ/faq", faqContext)
     }
+
+    func codeOfConductHandler(_ req: Request) throws -> Future<View> {
+        let cocContext = CodeOfConductContext()
+        return try req.view().render("App/YearX/Pages/CodeOfConduct/code-of-conduct", cocContext)
+    }
 }
 
 struct HomeContext: Encodable {
@@ -24,4 +30,8 @@ struct HomeContext: Encodable {
 
 struct FaqContext: Encodable {
     let page = ["faq": true]
+}
+
+struct CodeOfConductContext: Encodable {
+    let page = ["code-of-conduct": true]
 }
