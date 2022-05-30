ded01173fcfaa5480a6e77ff6f90a1edcf8fd515
Tim <0xtimc@gmail.com>
2022-05-30 17:35:45 +0100

    Copy over styles to make new site work

---
+++ new/Resources/App/YearX/javascript/hambuger-menu.js
@@ -0,0 +1,19 @@
+<script>
+
+  function toggleHamburger() {
+    const button = document.querySelector('.hamburger');
+    const nav = document.querySelector('.navigation');
+    
+    toggleClass(button);
+    toggleClass(nav);
+  }
+
+  function toggleClass(element) {
+    const isActiveClass = "is-active";
+    if (element.classList.contains(isActiveClass)) {
+      element.classList.remove(isActiveClass)
+    } else {
+      element.classList.add(isActiveClass)
+    }
+  }
+</script>
