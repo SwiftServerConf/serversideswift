dbebb91f168e7fa26b8d68b9c1d3e0f58b46c07c
Tim <0xtimc@gmail.com>
2024-02-26 12:49:54 +0000

    Package updates

--- Sources/Conference/Components/Tickets.swift
+++ Sources/Conference/Components/Tickets.swift
@@ -8,7 +8,7 @@ struct Tickets: Component {
                 Paragraph("Please note that tickets are non-refundable, but can be re-assigned to someone else.").class("text-center")
 //                Wrapper {
 //                    Node<HTML.BodyContext>.element(named: "tito-widget", attributes: [
-//                        Attribute<HTML.BodyContext>(name: "event", value: "serverside-swift/2022")
+//                        Attribute<HTML.BodyContext>(name: "event", value: "serverside-swift/2024")
 //                    ])
 //                }
                 Div {
