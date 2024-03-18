1bb3de6917fa6e3ce6a9de8fad424ead03c1e21c
Tim <0xtimc@gmail.com>
2024-03-18 15:22:18 +0000

    Add first video

--- Sources/Conference/Components/Videos.swift
+++ Sources/Conference/Components/Videos.swift
@@ -12,6 +12,23 @@ struct Videos: Component {
                         Link("choose a year", url: "/years")
                         Text(".")
                     }.class("text-center mt-4")
+                    
+                    /** Current yearr videos **/
+//                    H1("Videos").class("text-center text-white mb-4")
+//                    H4("Watch all the amazing content from the conference.").class("text-center mb-5")
+//                    List {
+//                        Div {
+//                            Div {
+//                                Div {
+//                                    IFrame(url: "https://www.youtube.com/embed/eHgpKdvXino", addBorder: false, allowFullScreen: true, enabledFeatureNames: ["autoplay", "encrypted-media"])
+//                                }.class("embed-container")
+//                            }.class("video")
+//                            Div {
+//                                H3("Panel Discussion").class("h3")
+//                            }.class("description p-2")
+//                        }.class("card")
+//                    }
+//                    H4("More videos coming soon...").class("text-center mb-5")
                 }.class("container")
             }.class("page-block no-height bg-blue text-white")
         }.class("videos")
